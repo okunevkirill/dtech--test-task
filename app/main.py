bd9dd3568a58edc5cfdedcc467692455ba2dfb34
ok_kir <clicktmin@gmail.com>
2022-11-11 18:34:41 +0400

    refactor: Change response from root handler
    I made a response in the form of html text with a link to the API documentation.

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import HTMLResponse
 
 app = FastAPI(
     title="dtech--test-task",
 )
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def root():
-    return {"message": "It's a live..."}
+    html_content = """
+            <html>
+                <head>
+                    <title>dtech--test-task--old</title>
+                </head>
+                <body>
+                    <h1>API по тестовому заданию</h1>
+                    <div>
+                        ✍ <a href="./docs">Документация приведена по данному адресу</a> 
+                    </div>
+                </body>
+            </html>
+            """
+    return HTMLResponse(content=html_content, status_code=status.HTTP_200_OK)
