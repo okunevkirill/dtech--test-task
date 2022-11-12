ce0c277ab56ceb2f2b3f205dc907729aaff69f03
ok_kir <clicktmin@gmail.com>
2022-11-12 15:26:16 +0400

    feat: Add CLI to application
    1. With the help of 'typer' the console interface is formed.
    2. Renamed FastAPI main object.
    3. Added database initialization command (CLI).
    4. Added command to start web service (CLI).

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
+import asyncio
+
+import typer
+import uvicorn
 from fastapi import FastAPI, status
 from fastapi.responses import HTMLResponse
 
-app = FastAPI(
+from app.settings import SETTINGS
+from app.db.base import init_models
+
+app_web = FastAPI(
     title="dtech--test-task",
 )
 
 
-@app.get("/", response_class=HTMLResponse)
+@app_web.get("/", response_class=HTMLResponse)
 async def root():
     html_content = """
             <html>
@@ -22,3 +29,25 @@ async def root():
             </html>
             """
     return HTMLResponse(content=html_content, status_code=status.HTTP_200_OK)
+
+
+# -----------------------------------------------------------------------------
+app_cli = typer.Typer(help=f"CLI for application {app_web.title!r}")
+
+
+@app_cli.command()
+def init_database():
+    """Database initialization - tables are being created."""
+    asyncio.run(init_models())
+    print("[*] Database initialized")
+
+
+@app_cli.command()
+def runserver():
+    """Launching an application with settings from the environment."""
+    uvicorn.run(app_web, host=SETTINGS.APP_HOST, port=SETTINGS.APP_PORT)
+
+
+# -----------------------------------------------------------------------------
+if __name__ == "__main__":
+    app_cli()
