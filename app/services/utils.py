edac7249e6bf8d95cb41d4f438b840c4ea8bd999
ok_kir <clicktmin@gmail.com>
2022-11-14 18:49:46 +0400

    feat: Add functions for working with a password in the database

---
+++ app/services/utils.py
@@ -0,0 +1,17 @@
+from passlib.context import CryptContext
+
+from app.settings import SETTINGS
+
+PASSWORD_CONTEXT = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_hashed_password(password: str) -> str:
+    salt = SETTINGS.APP_SECRET_KEY.get_secret_value()
+    password += salt
+    return PASSWORD_CONTEXT.hash(password)
+
+
+def verify_password(password: str, compared_hash: str) -> bool:
+    salt = SETTINGS.APP_SECRET_KEY.get_secret_value()
+    password += salt
+    return PASSWORD_CONTEXT.verify(password, compared_hash)
