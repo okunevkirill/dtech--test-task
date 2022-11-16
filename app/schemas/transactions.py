22bb33e9b975665a7d2863ce80ba43c27bff421d
ok_kir <clicktmin@gmail.com>
2022-11-16 17:15:49 +0400

    feat: Add schema to display transactions to admin

--- app/schemas/transactions.py
+++ app/schemas/transactions.py
@@ -1,4 +1,5 @@
 __all__ = [
+    "AdminTransactionOutputSchema",
     "TransactionOutputSchema",
 ]
 
@@ -22,6 +23,26 @@ class TransactionOutputSchema(BaseSchema):
             "example": {
                 "id": 1,
                 "amount": "42.00",
+                "bill_id": 1,
+                "created_at": "2022-11-13T15:14:58.657Z"
+            }
+        }
+
+
+class AdminTransactionOutputSchema(BaseSchema):
+    id: conint(ge=1)
+    amount: Decimal
+    user_id: conint(ge=1)
+    bill_id: conint(ge=1)
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "amount": "100",
+                "user_id": 1,
                 "bill_id": 1,
                 "created_at": "2022-11-13T15:14:58.657Z"
             }
